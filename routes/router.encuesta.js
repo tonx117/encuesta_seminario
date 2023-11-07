f139578fa6bea327fd0e9bc6c46ce89375c12751
Ivan Pietkiewicz <ivanciito_999@hotmail.com>
2023-11-07 15:05:02 -0300

    se arreglo unos errores

--- routes/router.encuesta.js
+++ routes/router.encuesta.js
@@ -1,6 +1,6 @@
 import express from "express";
 import encuestactrl from "../controllers/encuesta.controllers.js";
-import EncuestaSchema from "../validator/EncuestaSchema.js";
+import { EncuestaSchema } from "../validator/EncuestaSchema.js";
 import { validateSchema } from "../middlewares/Encuesta-validacion.js";
 
 const routes = express.Router();
