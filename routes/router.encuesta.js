7a808f1261f5bc5aa774d7d36d78b176a5a0503a
Ivan Pietkiewicz <ivanciito_999@hotmail.com>
2023-11-07 14:58:19 -0300

    Se agrago las validaciones

--- routes/router.encuesta.js
+++ routes/router.encuesta.js
@@ -1,8 +1,10 @@
 import express from "express";
 import encuestactrl from "../controllers/encuesta.controllers.js";
+import EncuestaSchema from "../validator/EncuestaSchema.js";
+import { validateSchema } from "../middlewares/Encuesta-validacion.js";
 
 const routes = express.Router();
 
-routes.post("/encuesta", encuestactrl.store);
+routes.post("/encuesta", EncuestaSchema, validateSchema, encuestactrl.store);
 
 export default routes;
---
+++ validator/EncuestaSchema.js
@@ -0,0 +1,84 @@
+import { checkSchema } from "express-validator";
+
+export const EncuestaSchema = checkSchema({
+    edad: {
+        in: ["body"],
+        notEmpty: true,
+        isInt: {
+            options: {
+                min: 0,
+                max: 100
+            }
+        }
+    },
+    localidadregistro: {
+        in: ["body"],
+        notEmpty: true,
+        isLength: {
+            options: {
+                min: 4,
+                max: 25
+            }
+        }
+    },
+    generomusicalregistro: {
+        in: ["body"],
+        notEmpty: true,
+        isLength: {
+            options: {
+                min: 3,
+                max: 25
+            }
+        }
+    },
+    cancionrecenteregistro: {
+        in: ["body"],
+        notEmpty: true,
+        isLength: {
+            options: {
+                min: 3,
+                max: 25
+            }
+        }
+    },
+    cancionfavoritaregistro: {
+        in: ["body"],
+        notEmpty: true,
+        isLength: {
+            options: {
+                min: 3,
+                max: 25
+            }
+        }
+    },
+    artistamusicalregistro: {
+        in: ["body"],
+        notEmpty: true,
+        isLength: {
+            options: {
+                min: 3,
+                max: 25
+            }
+        }
+    },
+    plataformamusicalregistro: {
+        in: ["body"],
+        notEmpty: true,
+        isLength: {
+            options: {
+                min: 3,
+                max: 25
+            }
+        }
+    },
+    tiempoescucharegistro: {
+        in: ["body"],
+        notEmpty: true,
+        isInt: {
+            options: {
+                min: 0,
+                max: 960
+            }
+        }
+    }
+})
